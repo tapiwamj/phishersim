3521a8de7cb00831187234d4e021d7af1ba203e6
tapiwamj <tmjanga@live.com>
2025-09-23 16:14:33 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const nunjucks = require("nunjucks");
+const port = 3000;
+const path = require("path");
+
+// Middleware
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.set("view engine", "njk");
+nunjucks.configure(path.join(__dirname, "views"), {
+  autoescape: true,
+  express: app,
+  watch: true,
+});
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+
+
