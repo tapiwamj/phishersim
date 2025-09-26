f9f822482974e8a56ccba6c1184ac15e844ac034
tapiwamj <tmjanga@live.com>
2025-09-26 13:58:57 +0100

    created story manager function and a routing to return emails to front end

--- src/storyManager.js
+++ src/storyManager.js
@@ -1,18 +1,74 @@
-const fs = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
+const sanitizeHtml = require("sanitize-html");
 
 class StoryManager {
   static SITEMAP = "";
-  constructor() {
-    fs.readFile(__dirname+"/emailstories/storymap.json", "utf8", (err, data) => {
-      if (err) {
-        console.error("Error reading file:", err);
-        return;
-      }
-      const json = JSON.parse(data);
-      console.log(data);
-      console.log("File contents:", data);
+  static STORY_DIR = path.join(__dirname, "emailstories", "mainstory");
+  constructor() {}
+  setRecepient() {}
+  async fetchStoryMap() {
+    try {
+      console.log(StoryManager.STORY_DIR);
+      const storypath = path.join(StoryManager.STORY_DIR, "storymap.json");
+      const data = await fs.readFile(storypath, "utf8"); // now this works
+      StoryManager.SITEMAP = JSON.parse(data);
+    } catch (err) {
+      console.error("Error reading file:", err);
+    }
+  }
+  cleanHTML(html) {
+    return sanitizeHtml(html, {
+      allowedTags: [
+        "p",
+        "ul",
+        "li",
+        "strong",
+        "em",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "br",
+      ],
+      allowedAttributes: {},
+      allowedSchemes: ["http", "https", "mailto"], // only safe URL schemes
     });
+  }
+  async getEmail(emailIndex) {
+    const emailDetails = StoryManager.SITEMAP.emails[emailIndex];
+    const recipientName = emailDetails.sender;
+    let body = await this.fetchEmailBody(emailDetails.path);
+    body = body.replace(/\[\/recepient\/\]/g, recipientName);
+    body = this.cleanHTML(body);
+    return {
+      sender: emailDetails.sender,
+      name: emailDetails.name,
+      subject: emailDetails.subject,
+      body: body
+    };
+  }
+  async fetchEmailBody(htmlpath) {
+    let html;
+    try {
+      const storypath = path.join(StoryManager.STORY_DIR, htmlpath);
+      html = await fs.readFile(storypath, "utf8"); // now this works
+    } catch (err) {
+      console.error("Error reading file:", err);
+    }
+    return html;
   }
 }
 
-const dd = new StoryManager();
+module.exports = new StoryManager();
+
+// const dd = new StoryManager();
+// async function run() {
+//   await dd.fetchStoryMap();
+//   const hello = await dd.getEmail();
+//   console.log(hello);
+
+// }
+// run();
